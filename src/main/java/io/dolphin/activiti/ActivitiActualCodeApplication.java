4ddab0ee41801cf38211c061bb6b16dc719a1e2c
钱亮 <ericql57@163.com>
2021-01-24 17:34:06 +0800

    activiti hello world代码提交

---
+++ src/main/java/io/dolphin/activiti/ActivitiActualCodeApplication.java
@@ -0,0 +1,13 @@
+package io.dolphin.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiActualCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiActualCodeApplication.class, args);
+    }
+
+}
