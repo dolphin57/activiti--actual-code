9eec4ebbc93dd8bb534c5183edd8ea9878c5e565
钱亮 <ericql57@163.com>
2021-03-23 08:17:31 +0800

    workflow提交

---
+++ 07-workflow/src/main/java/io/dolphin/workflow/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package io.dolphin.workflow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description:
+ * @Author: Eric Liang
+ * @Since: 2021-3-23 7:56
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
